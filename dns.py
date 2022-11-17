25b0c4d0aee081a00e96eae1d207c7ffc2c36360
Lucas Costa <lucasrc.rodri@gmail.com>
2022-11-17 12:40:43 -0300

    mais alguns ajustes

--- dns.py
+++ dns.py
@@ -2,5 +2,5 @@ import socket
 L = int(input())
 
 for i in range(0, L):
-    reversed_dns = socket.gethostbyaddr(input())
-    print(reversed_dns[0])
+    dns = socket.gethostbyname(input())
+    print(dns)
